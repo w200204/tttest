88eb127fa061baaf1bbae94632434b414ceb489b
w200204 <83171619+w200204@users.noreply.github.com>
2021-04-26 00:10:17 +0900

    Update test.py

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
+            target_price = get_target_price("KRW-BTC", 0.8)
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price:
                 krw = get_balance("KRW")
